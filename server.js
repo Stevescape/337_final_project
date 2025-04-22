1580c621909dbce5c7fa5141b100393544bb545c
munshon04 <munshon2004@gmail.com>
2025-04-22 12:26:25 -0700

    made nav bar functional

--- server.js
+++ server.js
@@ -9,6 +9,21 @@ app.get('/home', function(req, res){
     res.sendFile(path.join(rootFolder, 'index.html'))
 })
 
+app.get('/products', function(req, res){
+    res.sendFile(path.join(rootFolder, 'products.html'))
+})
+
+app.get('/login', function(req, res){
+    res.sendFile(path.join(rootFolder, 'login.html'))
+})
+
+app.get('/create_account', function(req, res){
+    res.sendFile(path.join(rootFolder, 'create_account.html'))
+})
+
+app.get('/about', function(req, res){
+    res.sendFile(path.join(rootFolder, 'about.html'))
+})
 app.listen(3000, function(){
     console.log('Server Running at localhost:3000')
 })
